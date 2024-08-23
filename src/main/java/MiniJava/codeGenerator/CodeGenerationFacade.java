6135987a373cc6c10433f3b344e24c037b0bae68
AnitaAlikhani <AnitaAlikhani12@gmail.com>
2024-08-23 23:22:13 +0330

    fix

--- src/main/java/MiniJava/codeGenerator/CodeGenerationFacade.java
+++ src/main/java/MiniJava/codeGenerator/CodeGenerationFacade.java
@@ -1,36 +1,52 @@
 package MiniJava.codeGenerator;
 
-import java.util.ArrayList;
+import MiniJava.semantic.symbol.Symbol;
+import MiniJava.semantic.symbol.SymbolTable;
+import MiniJava.semantic.symbol.SymbolType;
 
 public class CodeGenerationFacade {
     private CodeGenerator codeGenerator;
+    private Memory memory;
+    private SymbolTable symbolTable;
 
     public CodeGenerationFacade() {
-        this.codeGenerator = new CodeGenerator();
+        this.memory = new Memory();
+        this.symbolTable = new SymbolTable(memory);
+        this.codeGenerator = new CodeGenerator(symbolTable, memory);
     }
 
-    public void addVariable(String name, varType type) {
-        Address address = new Address(codeGenerator.getMemory().getNextAddress(), type);
-        codeGenerator.getMemory().addVariable(name, address);
+    public void addVariable(String className, String methodName, String varName, SymbolType type) {
+        symbolTable.setLastType(type);
+        symbolTable.addMethodLocalVariable(className, methodName, varName);
     }
 
-    public void generateOperation(String operation, String sourceName, String destinationName) {
-        Address source = codeGenerator.getMemory().getAddress(sourceName);
-        Address destination = codeGenerator.getMemory().getAddress(destinationName);
-        if (source != null && destination != null) {
-            Operation op = new Operation(operation, source, destination);
-            codeGenerator.addOperation(op);
+    public void generateAssignment(String className, String methodName, String variableName, int value) {
+        Symbol symbol = symbolTable.get(className, methodName, variableName);
+        if (symbol != null) {
+            Address varAddress = new Address(symbol.address, varType.Int, TypeAddress.Direct);
+            Address valueAddress = new Address(value, varType.Int, TypeAddress.Imidiate);
+            codeGenerator.generateAssignment(varAddress, valueAddress);
         } else {
-            System.err.println("Invalid source or destination for operation.");
+            System.err.println("Variable not found in symbol table.");
         }
     }
 
-    public ArrayList<Operation> getOperations() {
-        return codeGenerator.getOperations();
+    public void generateArithmeticOperation(String className, String methodName, String destVar, String srcVar1, String srcVar2, Operation operation) {
+        Symbol destSymbol = symbolTable.get(className, methodName, destVar);
+        Symbol srcSymbol1 = symbolTable.get(className, methodName, srcVar1);
+        Symbol srcSymbol2 = symbolTable.get(className, methodName, srcVar2);
+
+        if (destSymbol != null && srcSymbol1 != null && srcSymbol2 != null) {
+            Address destAddress = new Address(destSymbol.address, varType.Int, TypeAddress.Direct);
+            Address srcAddress1 = new Address(srcSymbol1.address, varType.Int, TypeAddress.Direct);
+            Address srcAddress2 = new Address(srcSymbol2.address, varType.Int, TypeAddress.Direct);
+            codeGenerator.generateOperation(destAddress, srcAddress1, srcAddress2, operation);
+        } else {
+            System.err.println("One or more variables not found in symbol table.");
+        }
     }
 
-    public void executeCode() {
-        codeGenerator.generateCode();
+    public void printGeneratedCode() {
+        memory.pintCodeBlock();
     }
-
 }
